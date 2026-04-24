a28caed0dd53c4332ab512813ee1562f7b63398f
Steve Jung <steve@lemoncloud.io>
2026-04-24 13:41:01 +0900

    chore: project setup

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    rules: {
+        // '@typescript-eslint/indent': ['error', 4],      // use tab width 4
+        '@typescript-eslint/indent': 'off',
+        '@typescript-eslint/camelcase': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        // Added rule after upgrade eslint v8 and typescript v4
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/prefer-as-const': 'off',
+    },
+};
---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+// Vitest configuration for Node-based test execution.
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    test: {
+        environment: 'node',
+        include: ['src/**/*.spec.ts'],
+        coverage: {
+            reporter: ['text', 'html'],
+        },
+    },
+});
